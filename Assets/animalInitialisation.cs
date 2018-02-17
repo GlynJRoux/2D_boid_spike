62f8c490f9ba21dac0de12ba2dae23912892c6a9
Glyn <gjr4@aber.ac.uk>
2018-02-17 18:28:28 +0000

    Rest of code that was not committed before due to forgetting to 'git add .'

---
+++ Assets/animalInitialisation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animalInitialisation : MonoBehaviour {
+
+	public GameObject[] birds;
+	public GameObject birdPrefab;
+	public int birdCounter = 10;
+	public Vector3 spawnRange = new Vector3(5, 5, 5);
+
+	[Range(0,150)]
+	public int neighbourDistance = 25;
+
+	[Range (0,10)]
+	public float maxForce = 1f;
+
+	[Range (0,2)]
+	public float maxVelocity = 3f;
+
+	// Use this for initialization
+	void Start () {
+		birds = new GameObject[birdCounter];
+		for(int i = 0; i < birdCounter; i++)
+		{
+			Vector3 birdPosition = new Vector3 (Random.Range (-spawnRange.x, spawnRange.x),
+				                  Random.Range (-spawnRange.y, spawnRange.y),
+				                  Random.Range (0, 0));
+			birds [i] = Instantiate (birdPrefab, this.transform.position + birdPosition, Quaternion.identity) as GameObject;
+			birds [i].GetComponent<boid> ().boidManager = this.gameObject;
+
+		}
+	}
+}
---
+++ Assets/boid.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boid : MonoBehaviour {
+	public GameObject boidManager;
+	public Vector2 location;
+	public Vector2 velocity;
+	Vector2 goalPos;
+	Vector2 currPos;
+	Vector2 currForce;
+
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector2 (Random.Range (0.1f, 0.1f), Random.Range (0.1f, 0.01f));
+		location = new Vector2 (this.transform.position.x, this.gameObject.transform.position.y);
+		
+	}
+	Vector2 findTarget (Vector2 target){
+		return (target - location);
+}
+	void applyForce(Vector2 force){
+		
+		Vector3 f = new Vector3 (force.x, force.y, 0);
+		if (f.magnitude > boidManager.GetComponent<animalInitialisation> ().maxForce) {
+			f = f.normalized;
+			f *= boidManager.GetComponent<animalInitialisation> ().maxForce;
+		}
+		this.GetComponent<Rigidbody2D> ().AddForce (f);
+
+		if (this.GetComponent<Rigidbody2D> ().velocity.magnitude > boidManager.GetComponent<animalInitialisation> ().maxVelocity) {
+			this.GetComponent<Rigidbody2D> ().velocity = this.GetComponent<Rigidbody2D> ().velocity.normalized;
+			this.GetComponent<Rigidbody2D> ().velocity *= boidManager.GetComponent<animalInitialisation> ().maxVelocity;
+		}
+
+	}
+
+
+
+	Vector2 AllignWithGroup(){
+		double neighbourDist = boidManager.GetComponent<animalInitialisation> ().neighbourDistance;
+		Vector2 sum = Vector2.zero;
+		int count = 0;
+		foreach (GameObject other in boidManager.GetComponent<animalInitialisation>().birds) {
+			if (other == this.gameObject)
+				continue;
+			double d = Vector2.Distance (location, other.GetComponent<boid> ().location);
+			if (d < neighbourDist) {
+				sum = other.GetComponent<boid> ().velocity;
+				count++;
+			}
+		}
+		if (count > 0) {
+			sum /= count;
+			Vector2 direction = sum - velocity;
+			return direction;
+		}
+		return Vector2.zero;
+		 
+	}
+	Vector2 CohesionOfGroup(){
+		double neighbourDist = boidManager.GetComponent<animalInitialisation> ().neighbourDistance;
+		Vector2 sum = Vector2.zero;
+		int count = 0;
+		foreach (GameObject other in boidManager.GetComponent<animalInitialisation>().birds) {
+			if (other == this.gameObject)
+				continue;
+			double d = Vector2.Distance (location, other.GetComponent<boid> ().location);
+			if (d < neighbourDist) {
+				sum += other.GetComponent<boid> ().location;
+				count++;
+			}
+		}
+		if (count > 0) {
+			sum /= count;
+			return findTarget (sum);
+		}
+		return Vector2.zero;
+
+	}
+	void flock(){
+		location = this.transform.position;
+		velocity = this.GetComponent<Rigidbody2D> ().velocity;
+
+		if (Random.Range (0, 10) <= 1) {
+			Vector2 allign = AllignWithGroup ();
+			Vector2 cohesion = CohesionOfGroup ();
+			Vector2 goalLocation;
+			goalLocation = findTarget (goalPos);
+			currForce = goalLocation + allign + cohesion;
+			currForce = currForce.normalized;
+		}
+
+
+		applyForce (currForce);
+	}
+
+		
+	
+	// Update is called once per frame
+	void Update () {
+		goalPos = boidManager.transform.position;
+		flock ();
+	}
+}
